6a654973126d3326d1482faee93a12caef97b4f5
dipeshasd <dipesh.sah@hotmail.com>
2022-02-17 02:09:28 -0600

    adding junit jar

--- src/JUnitTest.java
+++ src/JUnitTest.java
@@ -1,34 +1,31 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import  org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
+
 
 
 public class JUnitTest {
 
     @Test
-    @DisplayName("Should insert Data in Linked List")
-    void shouldInsertDataInList(){
+    public void shouldInsertDataInList(){
         SinglyLinkedList linkedList = new SinglyLinkedList();
         Data data = new Student("Ram");
         linkedList.insert(data);
         String name = linkedList.getTop().getData().getId();
-        Assertions.assertEquals("Ram",name);
+        Assert.assertEquals("Ram",name);
     }
 
     @Test
-    @DisplayName("Should find Data in Linked List")
-    void shouldFindDataInList(){
+    public void shouldFindDataInList(){
         SinglyLinkedList linkedList = new SinglyLinkedList();
         Data data = new Student("John");
         linkedList.insert(data);
         Student result = (Student) linkedList.find(data.getId());
-        Assertions.assertNotNull(result);
-        Assertions.assertEquals("John",result.getId());
+        Assert.assertNotNull(result);
+        Assert.assertEquals("John",result.getId());
     }
 
     @Test
-    @DisplayName("Should merge two Linked List")
-    void shouldMergeTwoList(){
+    public  void shouldMergeTwoList(){
         SinglyLinkedList firstDataList = new SinglyLinkedList();
         SinglyLinkedList secondDataList = new SinglyLinkedList();
         Data testData1 = new Student("Ram");
@@ -37,38 +34,37 @@ public class JUnitTest {
         secondDataList.insert(testData2);
         firstDataList.merge(secondDataList);
         Student result = (Student) firstDataList.find(testData2.getId());
-        Assertions.assertNotNull(result);
-        Assertions.assertEquals("Shyam",result.getId());
+        Assert.assertNotNull(result);
+        Assert.assertEquals("Shyam",result.getId());
     }
 
 
+
+
     @Test
-    @DisplayName("Creating a new tutor")
-    void shouldCreateNewTutor(){
+    public void shouldCreateNewTutor(){
         Validator validator = new Validator();
         validator.validate("TUTOR tutor2 15");
         int tutorListSize = validator.handler.service.tutorList.getSize();
-       Assertions.assertEquals(1, tutorListSize);
+        Assert.assertEquals(1, tutorListSize);
     }
 
     @Test
-    @DisplayName("Creating a new student")
-    void shouldCreateNewStudent(){
+    public void shouldCreateNewStudent(){
         Validator validator = new Validator();
         validator.validate("STUDENT test");
         int studentListSize = validator.handler.service.studentList.getSize();
-        Assertions.assertEquals(1, studentListSize);
+        Assert.assertEquals(1, studentListSize);
     }
 
     @Test
-    @DisplayName("Should add topic to tutor")
-     void shouldAddTopic(){
+    public void shouldAddTopic(){
         Validator validator = new Validator();
         validator.validate("TUTOR tutor1 10");
         validator.validate("TOPIC OO tutor1 10");
         Tutor tutor = (Tutor) validator.handler.service.tutorList.getTop().getData();
         String topicName = tutor.getTopics().getTop().getData().getId();
-        Assertions.assertEquals("OO",topicName );
+        Assert.assertEquals("OO",topicName );
     }
 
 
